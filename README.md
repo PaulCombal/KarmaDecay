2172ba92e1be8b5da72a4390ebb09ae4d8a12e0c
Paul <abonnementspaul@gmail.com>
2017-01-26 15:09:20 +0100

    Updated readme, added youtube links, added via=.. at theend of requests

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # KarmaDecay
 Unofficial Chrome extension for Reddit Karma Decay
 
-This extension is unofficial, and as long as this readme isnt updated, is not functionnal
+As of right now, this extension only supports imgur images and youtube videos.
+Note that this extension is compatible with RES
+
+It will add a small button like that next to all supported posts:
+![Preview of KarmaDecay extension](http://i.imgur.com/KHaUljp.png)
--- karma.js
+++ karma.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
 	//We select the posts that link to imgur
 	$(this).find('.link.thing').each(function(){
-		if ($(this).attr('data-domain') == "imgur.com") {
+		if ($(this).attr('data-domain') == "imgur.com" || 
+			$(this).attr('data-domain') == "youtube.com") {
 			//We find the url of the reddit post and add the karmadecay prefix
-			var fullurl = "http://karmadecay.com" + $(this).find("li.first a").attr("href").toString().substr($(this).find("li.first a").attr("href").indexOf("/r/"));
+			var fullurl = "http://karmadecay.com" + $(this).find("li.first a").attr("href").toString().substr($(this).find("li.first a").attr("href").indexOf("/r/")) + "?via=chromeExtension";
 
 			//We add the karmadecay link
 			$(this).find("ul").append('<li><a target="_blank" href="' + fullurl + '">KarmaDecay</a></li>');
