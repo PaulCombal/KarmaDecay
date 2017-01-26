68f8a2f5b01aae464f4e64e0f41bd7ea3c65a648
Paul <abonnementspaul@gmail.com>
2017-01-26 14:12:06 +0100

    added comments. Now functionnal for imgur posts

--- karma.js
+++ karma.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
-
+	//We select the posts that link to imgur
 	$(this).find('.link.thing').each(function(){
 		if ($(this).attr('data-domain') == "imgur.com") {
-			console.log($(this).attr('data-url'));
-			console.log($(this).find("ul").text());
-			$(this).find("ul").append('<li><a target="_blank" href="google.com">KarmaDecay</a></li>');
+			//We find the url of the reddit post and add the karmadecay prefix
+			var fullurl = "http://karmadecay.com" + $(this).find("li.first a").attr("href").toString().substr($(this).find("li.first a").attr("href").indexOf("/r/"));
+
+			//We add the karmadecay link
+			$(this).find("ul").append('<li><a target="_blank" href="' + fullurl + '">KarmaDecay</a></li>');
 		}
 	});
 
