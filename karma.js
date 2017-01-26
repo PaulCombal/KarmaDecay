09ac1db4dd6fc85b65a8ba1326de5ecb6eb09ca4
Paul <abonnementspaul@gmail.com>
2017-01-26 15:22:52 +0100

    Added i.imgur.com to the list of supported host sites

--- karma.js
+++ karma.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 	//We select the posts that link to imgur
 	$(this).find('.link.thing').each(function(){
 		if ($(this).attr('data-domain') == "imgur.com" || 
+			$(this).attr('data-domain') == "i.imgur.com" || 
 			$(this).attr('data-domain') == "youtube.com") {
 			
 			//We find the url of the reddit post and add the karmadecay prefix
