56ac2c2b86908e299978f1cebacd36c190c7f123
Paul <abonnementspaul@gmail.com>
2017-01-26 13:46:13 +0100

    Added karma button next to imgur posts

--- karma.js
+++ karma.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
 
-alert("LOOL");
+	$(this).find('.link.thing').each(function(){
+		if ($(this).attr('data-domain') == "imgur.com") {
+			console.log($(this).attr('data-url'));
+			console.log($(this).find("ul").text());
+			$(this).find("ul").append('<li><a target="_blank" href="google.com">KarmaDecay</a></li>');
+		}
+	});
 
 });
